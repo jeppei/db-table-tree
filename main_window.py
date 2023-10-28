1a2ac56c53842cf65cf0774bce433f718c3a5417
Jesper Lundin <jesper.l.lundin@gmail.com>
2023-10-28 17:21:58 +0200

    Add tabs to the window

--- main_window.py
+++ main_window.py
@@ -11,17 +11,20 @@ class MainWindow:
         self.table = table
         self.row_id = row_id
         self.env = env
-
         self.my_db_navigator = DBNavigator(DB(self.env))
-
         self.theme = Themes.superhero
 
         self.root = self.create_window()
-        self.combo_box = self.create_combo_box_for_tables()
-        self.text_box = self.create_text_input_for_row_id()
-        self.button = self.create_go_button()
+
+        tab1, tab2, tab3 = self.create_notebook(self.root)
+        self.table_explorer = tab1
+        self.database_search = tab2
+        self.settings = tab3
+
+        self.combo_box = self.create_combo_box_for_tables(tab1, table)
+        self.text_box = self.create_text_input_for_row_id(tab1, row_id)
+        self.button = self.create_go_button(tab1)
         self.tree = None
-        self.root.grid_rowconfigure(1, weight=1)
 
         self.root.mainloop()
 
@@ -29,35 +32,56 @@ class MainWindow:
         root = tkb.Window(themename=self.theme.name)
         root.geometry("1000x1000")
         root.title("Expandable Tree")
-        root.grid_rowconfigure(0, weight=1)
-        root.grid_columnconfigure(0, weight=1)
         root.grid_rowconfigure(0, weight=0)
+        root.grid_columnconfigure(0, weight=1)
         return root
 
-    def create_combo_box_for_tables(self):
+    def create_notebook(self, parent):
+        notebook = tkb.Notebook(parent)
+        notebook.pack(fill="both", expand=True)
+
+        tab1 = tkb.Frame(notebook)
+        tab1.grid_rowconfigure(1, weight=1)
+        tab1.grid_columnconfigure(0, weight=1)
+
+        tab2 = tkb.Frame(notebook)
+        tab2.grid_rowconfigure(1, weight=1)
+        tab2.grid_columnconfigure(0, weight=1)
+
+        tab3 = tkb.Frame(notebook)
+        tab3.grid_rowconfigure(1, weight=1)
+        tab3.grid_columnconfigure(0, weight=1)
+
+        notebook.add(tab1, text="Table explorer")
+        notebook.add(tab2, text="Database search")
+        notebook.add(tab3, text="Settings")
+
+        return tab1, tab2, tab3
+
+    def create_combo_box_for_tables(self, parent, table):
         all_table_names = self.my_db_navigator.get_all_table_names()
-        combo_box = tkb.Combobox(self.root, values=all_table_names)
+        combo_box = tkb.Combobox(parent, values=all_table_names)
         combo_box.grid(row=0, column=0, padx=0, pady=0, sticky="nsew")  # Use grid layout manager
-        if self.table in combo_box['values']:
-            combo_box.set(self.table)
+        if table in combo_box['values']:
+            combo_box.set(table)
         return combo_box
 
-    def create_text_input_for_row_id(self):
-        text_box = tkb.Text(self.root, height=1, width=25, wrap="none")
-        text_box.insert("1.0", self.row_id)
+    def create_text_input_for_row_id(self, parent, row_id):
+        text_box = tkb.Text(parent, height=1, width=25, wrap="none")
+        text_box.insert("1.0", row_id)
         text_box.grid(row=0, column=1, padx=0, pady=0, sticky="ew")  # Use grid layout manager
         text_box.bind("<Return>", self.on_enter_pressed)
         return text_box
 
-    def create_go_button(self):
-        button = tkb.Button(self.root, text="GO!", command=self.button_click)
+    def create_go_button(self, parent):
+        button = tkb.Button(parent, text="GO!", command=self.button_click)
         button.grid(row=0, column=2, padx=0, pady=0)  # Use grid layout manager
         return button
 
     def button_click(self):
         row_id = self.text_box.get("1.0", "end-1c")
         table = self.combo_box.get()
-        self.tree = Tree(self.root, self.theme, table, row_id, self.my_db_navigator)
+        self.tree = Tree(self.table_explorer, self.theme, table, row_id, self.my_db_navigator)
 
     def on_enter_pressed(self, _):
         if self.text_box == self.root.focus_get():
