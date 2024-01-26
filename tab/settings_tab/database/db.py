0b36245f300617fc3c1aedcf1d50c23547f162c6
Jesper Lundin <jesper.l.lundin@gmail.com>
2024-01-26 10:02:48 +0100

    Restructure code

--- tab/settings_tab/database/db.py
+++ tab/settings_tab/database/db.py
@@ -15,7 +15,7 @@ logging.getLogger('sqlalchemy').setLevel(logging.ERROR)
 
 
 def read_setting(param):
-    if param[0] == "$":
+    if param[0] == '$':
         return os.environ.get(param[1:])
     else:
         return param
@@ -23,7 +23,8 @@ def read_setting(param):
 
 class DB:
 
-    def __enter__(self):
+    def __init__(self, database_connection_settings: DatabaseConnectionSettings):
+        self.database_connection_settings = database_connection_settings
 
         self.name = read_setting(self.database_connection_settings.name.get())
         self.host = read_setting(self.database_connection_settings.host.get())
@@ -31,18 +32,27 @@ class DB:
         self.user = read_setting(self.database_connection_settings.user.get())
         self.password = read_setting(self.database_connection_settings.password.get())
         self.database = read_setting(self.database_connection_settings.database.get())
+
         self.table_to_column_dict = self.get_table_and_columns()
+
+    def __str__(self):
+        return(
+            f'connection details:\n'
+            f' - host: {self.host}\n'
+            f' - database: {self.database}\n'
+            f' - port: {self.port}\n'
+            f' - user: {self.user}\n'
+            f' - password is not None: {self.password is not None}\n'
+        )
+
+    def __enter__(self):
+
+
         #self.table_to_column_dict = {}
         #self.connected = False
         #self._connection = None
 
-        print(
-            f'Connecting to the {self.name} '
-            f'database on {self.host}/'
-            f'{self.database}:'
-            f'{self.port} '
-            f'with user {self.user}'
-        )
+        print(f'Connecting to the {self.name} with {self}')
         self._connection = mysql.connector.connect(
             host=self.host,
             port=self.port,
@@ -60,7 +70,7 @@ class DB:
         #        database=self.database
         #    )
         #    self._my_cursor = self._connection.cursor()
-        #    self._my_cursor.execute("SELECT 1")
+        #    self._my_cursor.execute('SELECT 1')
         #    self.table_to_column_dict = self.get_table_and_columns()
         #    self.connected = True
         #except Exception as e:
@@ -68,6 +78,12 @@ class DB:
         #    self.connected = False
 
         return self
+
+    def __exit__(self, *_):
+        self._connection.close()
+        #if self._connection is not None:
+        #    self._connection.close()
+        self._my_cursor = None
 
     def show_connection_error_popup(self, error_message: Exception):
         root = tk.Tk()
@@ -84,21 +100,12 @@ class DB:
         )
         print(message)
         #traceback.print_exc()
-        tk.messagebox.showinfo("Connection Error", message)
+        tk.messagebox.showinfo('Connection Error', message)
         root.destroy()
-
-    def __exit__(self, *_):
-        self._connection.close()
-        #if self._connection is not None:
-        #    self._connection.close()
-        self._my_cursor = None
-
-    def __init__(self, database_connection_settings: DatabaseConnectionSettings):
-        self.database_connection_settings = database_connection_settings
 
     def execute_query(self, query):
         if self._my_cursor is None:
-            raise ValueError("Dont query unless you connect")
+            raise ValueError('Dont query unless you connect')
         #if not self.connected or self._my_cursor is None:
         #    return [], []
 
@@ -107,21 +114,23 @@ class DB:
             self._my_cursor.execute(query)
         except mysql.connector.Error as error:
             if error.errno == 1146 and error.sqlstate == '42S02':
-                messagebox.showinfo("The table doesnt exist", "The selected table doesn't exist!")
+                messagebox.showinfo('The table doesnt exist', 'The selected table doesnt exist!')
                 return [], []
             else:
                 messagebox.showinfo(
-                    "Unknown error", "Unknown error while trying to execute the query\n\n" + query + "\n\n" + error.msg
+                    'Unknown error', 'Unknown error while trying to execute the query\n\n' + query + '\n\n' + error.msg
                 )
                 return [], []
 
         columns = [i[0] for i in self._my_cursor.description]
         return self._my_cursor.fetchall(), columns
 
+
     def get_table_and_columns(self):
         try:
             connection_string = f'mysql+pymysql://{self.user}:{self.password}@{self.host}/{self.database}'
-            print(f"Connecting to: {connection_string}")
+            print(f'Connecting with {self}')
+            print(f'Connection string: {connection_string}')
             engine = create_engine(connection_string)
             #engine = create_engine(f'mysql+pymysql://{self.user}:{self.password}@{self.host}/{self.database}')
 
@@ -137,5 +146,5 @@ class DB:
             return table_to_columns
 
         except Exception as ex:
-            print("Failed to get tables and columns")
+            print('Failed to get tables and columns')
             return{}
